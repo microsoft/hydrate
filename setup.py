e06953c87ddae15f83b589a53e713dae661c6780
Andrew Doing <asd5h4@mst.edu>
2019-06-25 17:43:30 -0700

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='hydrate',
-    version='0.3.0',
+    version='0.4.0',
     author='andrewDoing',
     author_email='andrew.doing@mst.edu',
     description='A package to generate an HLD for your kubernetes cluster',
