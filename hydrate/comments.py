82ab255723b277c91d5486a46cf4941a9154ade2
Andrew Doing <asd5h4@mst.edu>
2019-07-08 15:22:32 -0700

    Add comments.py file for comment constants

---
+++ hydrate/comments.py
@@ -0,0 +1,18 @@
+"""Home of component.yaml comment constants."""
+
+
+TOP_LEVEL_COMMENT = '''Automatically generated using hydrate.
+Repository link: https://github.com/microsoft/hydrate
+Fabrikate Docs: https://github.com/Microsoft/fabrikate
+For private repositories, check the following:
+Authentication: https://github.com/microsoft/fabrikate/blob/master/docs/auth.md
+For more information on how components are structured, check the following:
+Component Model: https://github.com/microsoft/fabrikate/blob/master/docs/component.md'''  # noqa
+
+
+NO_MATCH_COMMENT = '''No Match Deployments
+In order to use these deployments with Fabrikate, follow the steps below.
+1. Populate the source (git repository link)
+2. Add a path field (path within the git repository) ex: "path: stable/"
+3. Specify helm for applications generated using a Helm chart ex: "type: helm"
+Otherwise, comment them out to prevent errors with Fabrikate generation.'''
--- hydrate/hld.py
+++ hydrate/hld.py
@@ -1,17 +1,10 @@
 """Use to construct the High-Level Deployment."""
+from .comments import TOP_LEVEL_COMMENT
 from ruamel.yaml.comments import CommentedMap, CommentedSeq
 from ruamel.yaml import YAML
 yaml = YAML()
 
 OFFSET = 2
-
-TOP_LVL_COMMENT = '''Automatically generated using hydrate.
-Repository link: https://github.com/microsoft/hydrate
-Fabrikate Docs: https://github.com/Microsoft/fabrikate
-For private repositories, check the following:
-Authentication: https://github.com/microsoft/fabrikate/blob/master/docs/auth.md
-For more information on how components are structured, check the following:
-Component Model: https://github.com/microsoft/fabrikate/blob/master/docs/component.md''' # noqa
 
 
 def generate_HLD(component, output, comment_indeces=None):
@@ -28,7 +21,7 @@ def generate_HLD(component, output, comment_indeces=None):
     d = component.asdict()
     if comment_indeces:
         d = CommentedMap(d)
-        d.yaml_set_start_comment(TOP_LVL_COMMENT)
+        d.yaml_set_start_comment(TOP_LEVEL_COMMENT)
         lst = CommentedSeq(d["subcomponents"])
         for idx, comment in comment_indeces:
             lst.yaml_set_comment_before_after_key(idx, comment, OFFSET)
